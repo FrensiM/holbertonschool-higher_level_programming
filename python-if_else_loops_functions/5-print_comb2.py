c0f6f46ce255a486520aafed24d43715738387b2
FrensiM <frensimeta@gmail.com>
2023-05-30 05:54:21 -0700

    fixed

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for num in range(100):
-    print("{}, ".format(num), end="")
+    print("{:02d}, ".format(num), end="")
     if(num == 99):
         print("{}".format(num), end="")
