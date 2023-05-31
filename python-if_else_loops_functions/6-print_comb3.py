adc37954471edd54cacbaeef88b969ed3f12364e
FrensiM <frensimeta@gmail.com>
2023-05-31 01:16:30 -0700

    fixed

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num1 in range(0, 9):
-    for num2 in range(1,10):
+    for num2 in range(1, 10):
         if num1 + num2 == 17:
             print("{}{}".format(num1, num2))
         elif num1 != num2 and num1 < num2:
