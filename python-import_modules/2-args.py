f7413dc868e9eb07cc0ebc63a99581131d6b3f4b
FrensiM <frensimeta@gmail.com>
2023-06-01 13:21:55 +0200

    try

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import sys
+nr_arg = len(sys.argv) - 1
+for index, argument in enumerate(sys.argv[1:], start=1):
+    if nr_arg == 0:
+        print(f"{nr_arg} arguments.")
+    elif nr_arg == 1:
+        print(f"{nr_arg} argument:")
+        print(f"{index}: {argument}")
+    elif nr_arg != 0:
+        print(f"{nr_arg} arguments:")
+        print(f"{index}: {argument}")
