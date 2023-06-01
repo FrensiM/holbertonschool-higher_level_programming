176a9b1b7ecb5c42b373d859bd82a74cda72857d
FrensiM <frensimeta@gmail.com>
2023-06-01 12:42:41 +0200

    try

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 from add_0 import add
-<<<<<<< HEAD
-if __name__ == "main":
-=======
 if __name__ == "__main__":
->>>>>>> 1aedbcbd813f671b7df7b371a10e914e2757977b
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
