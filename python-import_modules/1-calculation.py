5383b496cfed1cfd0c020421659ab53fa027422a
FrensiM <frensimeta@gmail.com>
2023-06-01 12:32:44 +0200

    fixed

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == '__main__':
+    a = 10
+    b = 5
+print("{} + {} = {}".format(a, b , add(a, b)))
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
