d505a0782c189326541272e61b9fb8d46581f2cf
User <user@Users-MacBook.local>
2023-06-02 14:38:44 +0200

    fixed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    i = 1
+    sum = 0
+    while i < len(sys.argv):
+        sum += int(sys.argv[i])
+        i += 1
+    print(f"{sum}")
