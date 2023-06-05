8698150077dcaf6ffb2f9396eaf16142e0b6ca7e
User <user@Users-MacBook.local>
2023-06-05 14:04:06 +0200

    try

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if my_list is not None:
-    new_list = list(my_list)
-    new_list.reverse()
-    for elements in new_list:
-        print("{:d}".format(elements))
+        new_list = list(my_list)
+        new_list.reverse()
+        for elements in new_list:
+            print("{:d}".format(elements))
