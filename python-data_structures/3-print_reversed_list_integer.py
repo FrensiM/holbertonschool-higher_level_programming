4b0d229f71630e946f198b06270714904cd2c99a
User <user@Users-MacBook.local>
2023-06-05 13:57:48 +0200

    fixed

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == '0':
+        return None
     new_list = list(my_list)
     new_list.reverse()
     for element in new_list:
