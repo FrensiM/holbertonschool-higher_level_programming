bbcaa54a9f948ace6406a44dc3472ef9bd13c3d4
User <user@Users-MacBook.local>
2023-06-05 14:30:34 +0200

    fixed

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list is not None:
-        if idx < 0 or idx > len(my_list):
+        if idx < 0 or idx >= len(my_list):
             return my_list
         else:
             new_list = my_list.copy()
-            new_list.insert(idx, element)
-            new_list.pop(idx + 1)
+            new_list[idx] = element
             return new_list
