1ed4480b3c7adeb231efd2837e1448d4749272d9
User <user@Users-MacBook.local>
2023-06-06 13:27:12 +0200

    fixed

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0 or sentence == 0:
-        print("Length: {:d} - First character: None".format(len(sentence)))
-    else:
-        print("Length: {:d} - First character: {}".format(len(sentence), sentence[0]))
+    if len(sentence) == 0:
+        return 0, None
+    return len(sentence), sentence[0]
