ca0c4feee46f4e24f56fdfac4498e908c287e750
User <user@Users-MacBook.local>
2023-06-06 10:03:39 +0200

    try

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for num1 in matrix:
-        for num2 in range(len(num1)):
-            if num2 == len(num1) - 1:
-                print("{:d}".format(num1[num2]), end="")
-            else:
-                print("{:d}".format(num1[num2]), end="")
+    if len(matrix) == 0 or len(matrix[0]) == 0:
+        return None
+
+    max_element_length = len(str(max(max(matrix, key=max))))
+    row_format = "{:>" + str(max_element_length) + "}"
+
+    for row in matrix:
+        for element in row:
+            print(row_format.format(element), end=" ")
         print()
