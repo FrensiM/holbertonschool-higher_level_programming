369ec47c5431656dd828e23d385616726f6c82f8
User <user@Users-MacBook.local>
2023-06-08 14:17:25 +0200

    fixed

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    i = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+            count += 1
+        except ValueError:
+            i += 1
+            continue
+        except TypeError:
+            i += 1
+            continue
+        print()
+        return count
