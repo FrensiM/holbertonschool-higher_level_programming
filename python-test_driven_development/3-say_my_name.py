1226af3b50b86cb96f2a58fcfe99c9d332008882
User <user@Users-MacBook.local>
2023-06-15 14:21:28 +0200

    add

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Integer addition module """
+
+
+def say_my_name(first_name, last_name=""):
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
