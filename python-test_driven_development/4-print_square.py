367a293fd1a4303017ddf361f93331c44d982843
User <user@Users-MacBook.local>
2023-06-15 14:37:26 +0200

    fixed

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,7 +4,7 @@
 
 def print_square(size):
     '''printing a square'''
-    if type(size) is str:
+    if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
