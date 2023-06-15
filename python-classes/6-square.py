27b0a15f2bb2acd79c46b49f6aa041138d6928ed
User <user@Users-MacBook.local>
2023-06-15 10:14:24 +0200

    ficxed

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,52 +1,59 @@
 #!/usr/bin/python3
-'''square'''
+"""another square module """
 
 
 class Square:
-    '''init class'''
+    '''init func'''
     def __init__(self, size=0, position=(0, 0)):
         if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-        if type(position) is not tuple or position[0] < 0 or position[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = position
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        if type(position) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(position) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__size = size
+        self.__position = position
 
+    '''getter for size'''
     @property
     def size(self):
-        '''size'''
         return self.__size
 
-    @property
-    def position(self):
-        '''position'''
-        return self.__position
-
+    '''change size func'''
     @size.setter
     def size(self, value):
         if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
         self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
 
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
-
+        if type(value) is not tuple:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if len(value) != 2:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+    '''find area of square'''
     def area(self):
-        '''area'''
         return self.__size ** 2
 
     def my_print(self):
-        '''print'''
         if self.__size == 0:
             print()
             return
