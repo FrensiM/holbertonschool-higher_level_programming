44e7e3244d3d6a0e38586247af4cc3cef12afe50
User <user@Users-MacBook.local>
2023-06-15 14:23:25 +0200

    fixed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    '''gets a name and a surname if provided and prints the full name'''
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
