1b375be352a27cbdbaaa8698f27aabce61f34390
User <user@Users-MacBook.local>
2023-06-16 10:59:36 +0200

    fixed

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Print square module"""
+
+
+def text_indentation(text):
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    char = [".", "?", ":"]
+    el = 0
+    while el < len(text):
+        print(f"{text[el]}", end="")
+        if text[el] in char:
+            print()
+            print()
+            while el + 1 <= len(text) and text[el + 1] == " ":
+                el += 1
+        el += 1
