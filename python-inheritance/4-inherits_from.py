672e9c13b0a267004cd1da690b80065378248e1d
User <user@Users-MacBook.local>
2023-06-20 13:33:56 +0200

    fixed

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,8 @@
 
 def inherits_from(obj, a_class):
     '''fun that check if a obj is inherits'''
-    if isinstance(obj, a_class) or any(issubclass(type(obj), cls)
-                                       for cls in a_class.__subclasses__()):
+    if isinstance(obj, a_class):
+        if obj.__class__ is a_class:
+            return False
         return True
     return False
