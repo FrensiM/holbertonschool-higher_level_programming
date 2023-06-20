e715bce71f658d908a496327389e922d667ced9c
User <user@Users-MacBook.local>
2023-06-20 14:00:17 +0200

    try

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Module for a func'''
+
+
+class BaseGeometry:
+    '''empty class'''
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
