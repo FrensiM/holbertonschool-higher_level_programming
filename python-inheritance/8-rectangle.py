3b5038a79fdb4ddcd380d8439812ddf9d62d1361
User <user@Users-MacBook.local>
2023-06-21 09:59:07 +0200

    fixed

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 '''Module for a func'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
