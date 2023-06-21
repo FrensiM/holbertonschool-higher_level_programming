ab46ed684b2172c772d0b6d5607ec15fdef8c2f8
User <user@Users-MacBook.local>
2023-06-21 13:46:03 +0200

    fixeeeeeeeeeeeeeeeed

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,6 +4,6 @@
 
 def append_write(filename="", text=""):
     '''apending at the end func'''
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         fn = f.write(text)
     return fn
