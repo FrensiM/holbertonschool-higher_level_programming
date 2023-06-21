b159120103ceb517518c05ed239223c1faf3bdc2
User <user@Users-MacBook.local>
2023-06-21 10:28:23 +0200

    adding

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Module for a func'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    '''subclass'''
+    def __init__(self, size):
+        '''initialization'''
+        BaseGeometry.integer_validator(self, 'size', size)
+        Rectangle.__init__(self, size, size)
+        self.__size = size
+
+    def area(self):
+        '''area of square'''
+        return self.__size * self.__size
