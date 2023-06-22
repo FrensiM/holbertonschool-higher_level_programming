b11cfef84f3576627575bdc210c7b98be5158974
User <user@Users-MacBook.local>
2023-06-22 12:21:03 +0200

    try

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''reading text file'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''creating obj'''
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
