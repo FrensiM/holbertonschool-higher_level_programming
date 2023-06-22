6e896fc4186ee720409a5de195dcb7098615fd5c
User <user@Users-MacBook.local>
2023-06-22 12:45:33 +0200

    add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,8 +3,8 @@
 
 import os
 from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 if os.path.exists('add_item.json') is not True:
