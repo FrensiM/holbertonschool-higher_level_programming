e2b282ad5d9b8dbcb7bd686ed2af213b64bafdeb
User <user@Users-MacBook.local>
2023-06-23 09:54:13 +0200

    add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-'''Module for loading, adding and saving'''
+'''module to create a JSON file'''
+
 
 import os
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if os.path.exists('add_item.json') is not True:
-    with open('add_item.json', 'w', encoding='utf-8') as f:
-        load = []
-        save_to_json_file(load, 'add_item.json')
-load = load_from_json_file('add_item.json')
-for i in range(len(argv)):
+if os.path.exists("add_item.json") is not True:
+    with open("add_item.json", "w", encoding='utf-8') as file:
+        my_list = []
+        save_to_json_file(my_list, "add_item.json")
+my_list = load_from_json_file('add_item.json')
+for i in range(len(sys.argv)):
     if i != 0:
-        load.append(argv[i])
-save_to_json_file(load, 'add_item.json')
+        my_list.append(sys.argv[i])
+save_to_json_file(my_list, 'add_item.json')
