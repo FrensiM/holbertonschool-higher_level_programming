6bcd5e94f954d91bd7d5616aac00d192d5e504ca
User <user@Users-MacBook.local>
2023-06-26 16:44:11 +0200

    fixed

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,35 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @width.setter
+    def width(self, width):
+        self.__width = width
+
+    @height.setter
+    def height(self, height):
+        self.__height = height
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
