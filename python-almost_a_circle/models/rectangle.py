1d157b58586ae9c69716674e2f383cbd1fdbad5d
User <user@Users-MacBook.local>
2023-06-27 01:00:46 +0200

    try

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,34 +1,21 @@
 #!/usr/bin/python3
 '''class that inherits'''
 
-
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''class rectngle'''
+    '''class rectangle'''
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
     def width(self):
         return self.__width
-
-    @property
-    def height(self):
-        return self.__height
-
-    @property
-    def x(self):
-        return self.__x
-
-    @property
-    def y(self):
-        return self.__y
 
     @width.setter
     def width(self, width):
@@ -38,6 +25,10 @@ class Rectangle(Base):
             raise ValueError("width must be > 0")
         self.__width = width
 
+    @property
+    def height(self):
+        return self.__height
+
     @height.setter
     def height(self, height):
         if not isinstance(height, int):
@@ -46,14 +37,26 @@ class Rectangle(Base):
             raise ValueError("height must be > 0")
         self.__height = height
 
+    @property
+    def x(self):
+        return self.__x
+
     @x.setter
     def x(self, x):
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
         if x < 0:
             raise ValueError("x must be >= 0")
         self.__x = x
 
+    @property
+    def y(self):
+        return self.__y
+
     @y.setter
     def y(self, y):
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
@@ -63,7 +66,7 @@ class Rectangle(Base):
         return self.__height * self.__width
 
     def display(self):
-        '''print the square wiht #'''
+        '''print the square with #'''
         for i in range(self.__height):
             for j in range(self.__width):
                 print('#', end="")
