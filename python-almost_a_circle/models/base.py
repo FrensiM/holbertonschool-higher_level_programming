32422bc1693445d9d6740765a34801aa6763ae67
User <user@Users-MacBook.local>
2023-06-28 17:19:40 +0200

    add

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -23,3 +23,11 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''func'''
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
