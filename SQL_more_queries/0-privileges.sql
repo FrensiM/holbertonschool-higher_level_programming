c9359a4aed6a890fc19ed11e14620dbec3460bd6
User <user@Users-MacBook.local>
2023-07-26 10:32:06 +0200

    try

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- shows grants for all users
-SHOW GRANTS FOR "user_0d_1"@"localhost";
-SHOW GRANTS FOR "user_0d_2"@"localhost";
+-- script that shows grants for all users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
