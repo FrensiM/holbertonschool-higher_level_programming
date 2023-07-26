4ac23e68a12af2cb7e4711c0fb62b10c8319672f
User <user@Users-MacBook.local>
2023-07-26 11:13:13 +0200

    read user

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- crete db and user
-CREATE DATABASE IF NOT EXIST hbtn_0d_2;
-CREATE USER IF NOT EXIST 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
