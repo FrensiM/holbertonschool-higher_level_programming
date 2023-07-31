a3a92f621082b1e55dcdba95a0b031e589468785
User <user@Users-MacBook.local>
2023-07-31 12:26:12 +0200

    filter state

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa"""
-from pathlib import Path
-import sys
+"""script that lists all states with a name
+starting with N from the database hbtn_0e_0_usa"""
 import MySQLdb
+import sys
 
 
 if __name__ == '__main__':
