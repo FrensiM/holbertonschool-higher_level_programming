60ad230893e7c7073e9adda6f2da5dc58d1b2592
User <user@Users-MacBook.local>
2023-07-31 12:05:50 +0200

    try

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-'''script that list all state from db hbtn_0e_0_usa'''
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 
 if __name__ == '__main__':
-    
-    db = MySQLdb.connect(host="localhost", port="3306",
-                         user="sys.argv[1]", passwd="sys.argv[2]",
-                         db="sys.argv[3]", charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM state ORDER BY state.id")
-    res =cur.fetchall()
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], charset="utf8")
+    cr = db.cursor()
+    cr.execute("SELECT * FROM states ORDER BY states.id")
+    res = cr.fetchall()
     for rows in res:
         print(rows)
-    cur.close()
+    cr.close()
     db.close()
