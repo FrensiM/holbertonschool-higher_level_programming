76a8f6f47d557538e8cf6d77936f1c04c066fdad
User <user@Users-MacBook.local>
2023-08-01 13:09:04 +0200

    add

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""model state class"""
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+import sys
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(u, p, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    state = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    print("{}: {}".format(state.id, state.name))
+    session.close()
