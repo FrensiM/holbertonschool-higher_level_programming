8e2405f05793bff6081e4c30ecf306b36c3133a7
User <user@Users-MacBook.local>
2023-08-01 12:30:18 +0200

    adding

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,4 +9,10 @@ if __name__ == "__main__":
     u = sys.argv[1]
     p = sys.argv[2]
     db = sys.argv[3]
-    engine = create_
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(u, p, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
