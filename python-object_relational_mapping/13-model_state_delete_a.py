83299e43ae9ad56de2d9184ab73d6fe38e4bc508
User <user@Users-MacBook.local>
2023-08-01 14:28:08 +0200

    add

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""model state class"""
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+import sys
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(u, p, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    result = session.query(State).filter(State.name.like('%a%')).all()
+    for i in result:
+        session.delete(i)
+    session.commit()
+    session.close()
