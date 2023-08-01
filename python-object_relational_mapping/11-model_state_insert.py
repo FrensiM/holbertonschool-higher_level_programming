c79eded4160db7807e9edb70ceb8b475713f873e
User <user@Users-MacBook.local>
2023-08-01 13:29:33 +0200

    adding

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""model state class"""
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+import sys
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(u, p, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    my_state = State(name='Louisiana')
+    session.add(my_state)
+    session.commit()
+    print(f"{my_state.id}")
+    session.close()
+    
