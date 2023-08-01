1f1ed8a22e432327924b3e13effb1ff56f62bd23
User <user@Users-MacBook.local>
2023-08-01 13:36:59 +0200

    add

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""model state class"""
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+import sys
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(u, p, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    result = session.query(State).filter(State.id==2).update({'name':'New Mexico'})
+    session.commit()
+    session.close()
